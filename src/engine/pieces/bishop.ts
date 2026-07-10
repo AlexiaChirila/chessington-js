9e88ff16c8344900d4c0302b36a8b56923bf1ca3
AlexiaChirila <Alexia.Chirila@softwire.com>
2026-07-10 14:39:29 +0300

    Added Move to Bishop Piece.

--- src/engine/pieces/bishop.ts
+++ src/engine/pieces/bishop.ts
@@ -1,6 +1,7 @@
 import Piece from './piece';
 import Player from '../player';
 import Board from '../board';
+import Square from "../square";
 
 export default class Bishop extends Piece {
     public constructor(player: Player) {
@@ -8,6 +9,26 @@ export default class Bishop extends Piece {
     }
 
     public getAvailableMoves(board: Board) {
-        return new Array(0);
+        let position:Square=board.findPiece(this);
+        let moves:Square[] = [];
+
+        const directions=[[1,-1],[1,1],[-1,1],[-1,-1]];
+
+        for( const [dr,dc] of directions)
+        {
+            for(let i=1;i<8;i++)
+            {
+                let r=position.row +dr*i;
+                let c=position.col +dc*i;
+
+                if(r<0 || r>=7 || c<0 ||c>7)
+                    break;
+
+                moves.push(new Square(r,c));
+
+            }
+        }
+
+        return moves;
     }
 }
