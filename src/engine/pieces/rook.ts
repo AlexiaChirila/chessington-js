f896642e57a085f5899052682c79157864142a1c
AlexiaChirila <Alexia.Chirila@softwire.com>
2026-07-10 13:20:39 +0300

    Added Move to Rook Piece.

--- src/engine/pieces/rook.ts
+++ src/engine/pieces/rook.ts
@@ -1,6 +1,7 @@
 import Piece from './piece';
 import Player from '../player';
 import Board from '../board';
+import Square from "../square";
 
 export default class Rook extends Piece {
     public constructor(player: Player) {
@@ -8,6 +9,20 @@ export default class Rook extends Piece {
     }
 
     public getAvailableMoves(board: Board) {
-        return new Array(0);
+        let position:Square=board.findPiece(this);
+        let moves:Square[] = [];
+
+        for(let i=0;i<=7;i++)
+        {
+            if(i!==position.row)
+            {
+                moves.push(new Square(i,position.col));
+            }
+            if(i!==position.col)
+            {
+                moves.push(new Square(position.row,i));
+            }
+        }
+        return moves;
     }
 }
